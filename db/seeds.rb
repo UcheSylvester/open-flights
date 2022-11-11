f536ad4cc2473f7bdc3c69e96468fde9704887d7
UcheSylvester <okorocode@gmail.com>
2022-11-11 07:12:08 +0100

    chores: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airlines = Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png",
+    description: 'Best airline in the world'
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png",
+    description: 'Best airline in the world'
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png",
+    description: 'Best airline in the world'
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png",
+    description: 'Best airline in the world'
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png",
+    description: 'Best airline in the world'
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png",
+    description: 'Best airline in the world'
+  }
+])
+
+reviews = Review.create([
+  {
+    title: "Great airline",
+    description: "I had a lovely time.",
+    score: 5,
+    airline: airlines.first
+  },
+  {
+    title: "Bad airline",
+    description: "I had a bad time.",
+    score: 1,
+    airline: airlines.first
+  },
+])
